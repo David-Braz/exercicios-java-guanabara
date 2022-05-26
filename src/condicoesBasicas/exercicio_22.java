11f755e6556675a870bd9801c224cce47cd6e619
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-05-26 19:57:51 -0300

    Resolução de lista de exercícios - Prof. Gustavo Guanabara.

---
+++ src/condicoesBasicas/exercicio_22.java
@@ -0,0 +1,34 @@
+package condicoesBasicas;
+
+import java.util.Scanner;
+
+public class exercicio_22 {
+
+	public static void main(String[] args) {
+		
+		/* 22) Escreva um programa que leia o ano de nascimento de um rapaz
+		 * e mostre a sua situação em relação ao alistamento militar.
+		 * - Se estiver antes dos 18 anos, mostre em quantos anos faltam para o alistamento.
+		 * - Se já tiver depois dos 18 anos, mostre quantos anos já se passaram do alistamento.
+		 */
+		
+		int ano, idade, anosRestantes, anosPassados;
+				
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o ano de nascimento: ");
+		ano = scan.nextInt();
+		
+		idade = 2022 - ano;
+		
+		if(idade >= 18) {
+			anosPassados = idade - 18;
+			System.out.println("Já se passaram " + anosPassados + " anos desde o seu alistamento militar.");
+		}
+		else if(idade < 18) {
+			anosRestantes = 18 - idade;
+			System.out.println("Faltam " + anosRestantes + "anos restantes para o seu alistamento militar.");
+		}
+	}
+
+}
---
+++ src/condicoesBasicas/exercicio_24.java
@@ -0,0 +1,13 @@
+package condicoesBasicas;
+
+public class exercicio_24 {
+
+	public static void main(String[] args) {
+
+		/* 24) Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em Km.
+		 * Calcule o preço da passagem, cobrando R$0.50 por Km para viagens até 200Km e R$0.45 para viagens mais longas.
+		 */
+
+	}
+
+}
