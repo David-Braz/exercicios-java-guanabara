3612874cc2934bdbc1e6260b00b7836bf9ca630a
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-06-04 12:29:29 -0300

    Resolução de lista de exercícios - Prof. Gustavo Guanabara.

---
+++ src/repeticoesEnquanto/exercicio_42.java
@@ -0,0 +1,36 @@
+package repeticoesEnquanto;
+
+import java.util.Scanner;
+
+public class exercicio_42 {
+
+	public static void main(String[] args) {
+
+		/* 42) Faça um algoritmo que pergunte ao usuário um número inteiro e positivo
+		 * qualquer e mostre uma contagem até esse valor:
+		 * 
+		 * Ex: Digite um valor: 35
+		 * 
+		 * Contagem: 1 2 3 4 5 6 7 ... 33 34 35 Acabou!
+		 */
+		
+		int x, cont;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro e positivo: ");
+		cont = scan.nextInt();
+		
+		for (x = 1; x <= cont; x++) {
+			
+			System.out.print(x + " ");
+			
+			if(x == cont) {
+				
+				System.out.println("Acabou!");
+			}
+			
+		}
+	}
+
+}
