7e2b532f0879d40d7585c03890c1844e3b008665
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-06-06 01:14:00 -0300

    Resolução de lista de exercícios - Prof. Gustavo Guanabara.

---
+++ src/repeticoesEnquanto/exercicio_51.java
@@ -0,0 +1,49 @@
+package repeticoesEnquanto;
+
+import java.util.Scanner;
+
+public class exercicio_51 {
+
+	public static void main(String[] args) {
+
+		/*51) Faça um aplicativo que leia o preço de 8 produtos.
+		 * No final, mostre na tela qual foi o maior e qual foi o menor preço digitados.
+		 */
+		
+		double maiorValor = 0;
+		
+		double[] valores = new double[8];
+		Scanner scan = new Scanner(System.in);
+		
+		for(int x = 0; x <= 7; x++) {
+			
+			System.out.print("Digite o valor do produto: ");
+			valores[x] = scan.nextDouble();
+			
+			if(valores[x] > maiorValor) {
+				
+				maiorValor = valores[x];
+		
+			}
+		
+		}
+		
+		double menorValor = valores[0];
+		
+		for(int x = 0; x <= 7; x++) {
+			
+			if(valores[x] < menorValor) {
+				
+				menorValor = valores[x];
+				
+			}
+			
+			
+		}
+		
+		System.out.print("\nO maior valor digitado foi: " + maiorValor);
+		System.out.print("\nO menor valor digitado foi: " + menorValor);
+		
+	}
+
+}
---
+++ src/repeticoesEnquanto/exercicio_54.java
@@ -0,0 +1,59 @@
+package repeticoesEnquanto;
+
+import java.util.Scanner;
+
+public class exercicio_54 {
+
+	public static void main(String[] args) {
+
+		/* 54) Desenvolva um aplicativo que leia o peso e a altura de 7 pessoas, mostrando no final:
+		 * 
+		 * a) Qual foi a média de altura do grupo
+		 * b) Quantas pessoas pesam mais de 90Kg
+		 * c) Quantas pessoas que pesam menos de 50Kg tem menos de 1.60m
+		 * d) Quantas pessoas que medem mais de 1.90m pesam mais de 100Kg.
+		 */
+		
+		double peso, altura, mediaAltura, totalAltura = 0.0;
+		int contPesoAcimaNoventa = 0, contPesoMenosCinquenta = 0, contPesoAcimaCem = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		for(int x = 0; x <= 6; x++) {
+			
+			System.out.println("Pessoa " + (x+1));
+			System.out.print("\n\nQual é o seu Peso?\nDigite aqui: ");
+			peso = scan.nextDouble();
+			System.out.print("\nQual sua Altura?\nDigite aqui: ");
+			altura = scan.nextDouble();
+			System.out.println("\n===============================\n");
+			totalAltura += altura;
+			
+			if(peso > 90.0) {
+				
+				contPesoAcimaNoventa++;
+				
+				if(altura > 190.0 && peso > 100.0) {
+					
+					contPesoAcimaCem++;
+				}
+				
+			}
+			
+			else if(peso < 50.0 && altura < 190.0) {
+				
+				contPesoMenosCinquenta++;
+			}
+			
+		}
+		
+		mediaAltura = totalAltura/7;
+		System.out.println("\n========== R E S U L T A D O =========\n");
+		System.out.println("Altura média do grupo: " + mediaAltura + "m");
+		System.out.println("Quantidade de pessoas que pesam mais de 90Kg: " + contPesoAcimaNoventa);
+		System.out.println("Quantidade de pessoas que pesam menos de 50Kg e tem menos de 1.60m: " + contPesoMenosCinquenta);
+		System.out.println("Quantidade de pessoas que medem mais de 1.90m e pesam mais de 100Kg: " + contPesoAcimaCem);
+		
+	}
+
+}
