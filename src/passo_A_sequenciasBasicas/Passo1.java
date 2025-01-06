af578947d77b5c4facbe4df4ef17ff60b3b45b78
David Braz <davidbrazdasilva@gmail.com>
2025-01-06 17:01:35 -0300

    Inicio do Desafio.

---
+++ src/passo_A_sequenciasBasicas/Passo1.java
@@ -0,0 +1,10 @@
+package passo_A_sequenciasBasicas;
+
+public class Passo1 {
+
+    public static void main(String [] args){
+
+        System.out.print("Olá Mundo !!");
+
+    }
+}
---
+++ src/passo_A_sequenciasBasicas/Passo2.java
@@ -0,0 +1,14 @@
+package passo_A_sequenciasBasicas;
+
+
+import java.util.Scanner;
+
+public class Passo2 {
+    public static void main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite Seu Nome: ");
+        String Nome = scanner.nextLine();
+        System.out.print("Sejá Bem vindo: " + Nome + " Prazer em Conhecer !!");
+    }
+}
